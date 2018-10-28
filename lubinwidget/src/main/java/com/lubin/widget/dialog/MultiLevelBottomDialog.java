42e7555e069346d0d1f656bb5b3aa92224edfd46
靖升 <wang_junjie007@163.com>
2018-10-28 22:31:59 +0800

    read

--- lubinwidget/src/main/java/com/lubin/widget/dialog/MultiLevelBottomDialog.java
+++ lubinwidget/src/main/java/com/lubin/widget/dialog/MultiLevelBottomDialog.java
@@ -1,13 +1,24 @@
 package com.lubin.widget.dialog;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * @author lubin 2018/10/25
  * <p>
- * 多界别选择，类似京东地址选择器
+ * 多级别选择，类似京东app地址选择器
  * <p>
- * Multi domain selection, similar to Jingdong address selector
+ * Multi domain selection, similar to Jingdong APP address selector
  */
 public class MultiLevelBottomDialog extends DialogFragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
