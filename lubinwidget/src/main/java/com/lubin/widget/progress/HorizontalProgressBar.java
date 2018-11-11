1746b6cf388845448a8448247c475521ac6b0f42
靖升 <wang_junjie007@163.com>
2018-11-11 21:27:01 +0800

    progressBar

---
+++ lubinwidget/src/main/java/com/lubin/widget/progress/HorizontalProgressBar.java
@@ -0,0 +1,24 @@
+package com.lubin.widget.progress;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ProgressBar;
+
+/**
+ * @author lubin
+ * @version 1.0 2018
+ * 水平进度条
+ */
+public class HorizontalProgressBar extends ProgressBar {
+    public HorizontalProgressBar(Context context) {
+        super(context);
+    }
+
+    public HorizontalProgressBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public HorizontalProgressBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
---
+++ lubinwidget/src/main/java/com/lubin/widget/tools/DbSize.java
@@ -0,0 +1,15 @@
+package com.lubin.widget.tools;
+
+import android.content.Context;
+
+public class DbSize {
+    static int dp2px(Context context, int dpVal) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpVal * scale + 0.5f);
+    }
+
+    static int sp2px(Context context, int spVal) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spVal * fontScale + 0.5f);
+    }
+}
